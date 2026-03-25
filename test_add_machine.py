993b88b25e55335f9d270118cd16359f8fbbb18b
alfierijulien2004-netizen <alfierijulien2004@gmail.com>
2026-03-25 15:27:49 +0100

    Test ui

---
+++ test_add_machine.py
@@ -0,0 +1,19 @@
+from PyQt6 import QtCore, QtWidgets, uic
+import sqlite3
+import sys
+
+class AddMachine(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        ui_path = QtCore.QFileInfo(__file__).absolutePath() + "/Add_machine.ui"
+        ui_file_info = QtCore.QFileInfo(ui_path) #Crée un objet qui contient des informations du path car .exists() a besoin d'un objet QFileInfo pour vérifier l'existence du fichier
+        if not ui_file_info.exists(): 
+            print(f"Error: UI file not found at {ui_path}") #Revois une Error si le fichier n'existe pas
+        uic.loadUi(ui_path, self)
+        self.setWindowTitle("Add Machine")
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = AddMachine()
+    window.show()
+    sys.exit(app.exec())
