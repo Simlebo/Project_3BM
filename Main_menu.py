248535aec8c59d9e40c285d9213d300d30d59666
Simlebo <23012@ecam.be>
2026-03-27 13:46:22 +0100

    add files

--- Main_menu.py
+++ Main_menu.py
@@ -2,10 +2,10 @@ from PyQt6 import QtCore, QtWidgets, uic
 import sqlite3
 import sys
 
-class AddMachine(QtWidgets.QDialog):
+class Main_menu(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
-        ui_path = QtCore.QFileInfo(__file__).absolutePath() + "/Add_machine.ui"
+        ui_path = QtCore.QFileInfo(__file__).absolutePath() + "/Main_menu.ui"
         ui_file_info = QtCore.QFileInfo(ui_path) #Crée un objet qui contient des informations du path car .exists() a besoin d'un objet QFileInfo pour vérifier l'existence du fichier
         if not ui_file_info.exists(): 
             print(f"Error: UI file not found at {ui_path}") #Renvois une Error si le fichier n'existe pas
@@ -14,6 +14,6 @@ class AddMachine(QtWidgets.QDialog):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    window = AddMachine()
+    window = Main_menu()
     window.show()
     sys.exit(app.exec())
