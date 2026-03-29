a1aef13540f451639eb8d958b8aff7d298a26301
Simlebo <23012@ecam.be>
2026-03-29 15:43:55 +0200

    ca marche pas

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 from dB_3BM_Project import select_fabrication_step_number
-
+from dB_3BM_Project import select_fabrication_step
+g=select_fabrication_step("step_id = (SELECT MAX(step_id) FROM fabrication_step)")
+f = g[0]
+print(f)
 print(select_fabrication_step_number())
