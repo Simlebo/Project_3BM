80b1c072ba427ff4844bbdb325d13b077c0f808e
Simlebo <23012@ecam.be>
2026-03-29 13:13:40 +0200

    add pieces marche normalement

--- Add_Pieces.py
+++ Add_Pieces.py
@@ -2,12 +2,15 @@ from PyQt6 import QtCore, QtWidgets, uic
 import sqlite3
 import sys
 
+from dB_3BM_Project import insert_fabrication_step
+
 class Add_Pieces(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         ui_path = QtCore.QFileInfo(__file__).absolutePath() + "/Add_Pieces.ui"
         uic.loadUi(ui_path, self)
 
+        self.combobox()
         self.setWindowTitle("Add Pieces")
         self.Exit_Menu.clicked.connect(self.back_to_main_menu)
         self.Add_pieces.connect(self.add_pieces_to_db)
@@ -22,10 +25,10 @@ class Add_Pieces(QtWidgets.QDialog):
     def combobox(self):
         from dB_3BM_Project import select_machine
         rows = select_machine("")
-        for machine_id, name in rows:
-            self.Machine_combo_1.addItem(name, machine_id)
-            self.Machine_combo_2.addItem(name, machine_id)
-            self.Machine_combo_3.addItem(name, machine_id)
+        for machine_id in rows:
+            self.Machine_combo_1.addItem(machine_id)
+            self.Machine_combo_2.addItem(machine_id)
+            self.Machine_combo_3.addItem(machine_id)
 
 
 
@@ -33,6 +36,7 @@ class Add_Pieces(QtWidgets.QDialog):
         from dB_3BM_Project import insert_product
         from dB_3BM_Project import select_product
         from dB_3BM_Project import insert_fabrication_step
+        from dB_3BM_Project import select_fabrication_step
 
         rows = select_product("")
         product_id = len(rows) + 1
@@ -40,7 +44,30 @@ class Add_Pieces(QtWidgets.QDialog):
         price = float(self.Piece_Price.toPlainText())
         price_brut = float(self.Piece_Price_brut.toPlainText())
 
+        len = select_fabrication_step("")
+        step_id = len + 1
+        machine_id = float(self.Machine_combo_1.currentText())
+        time = float(self.Time_1.toPlainText())
+        step_name = self.Process_name_1.toPlainText()
+        fabrication_step = 1
+        insert_fabrication_step(step_id,product_id,machine_id,fabrication_step,time,step_name)
+        
+        if self.Process_name_2.toPlainText() != "":
+            step_id = step_id + 1
+            machine_id = float(self.Machine_combo_2.currentText())
+            time = float(self.Time_2.toPlainText())
+            step_name = self.Process_name_2.toPlainText()
+            fabrication_step = fabrication_step + 1
+            insert_fabrication_step(step_id,product_id,machine_id,fabrication_step,time,step_name)
 
+        if self.Process_name_3.toPlainText() != "":
+            step_id = step_id + 1
+            machine_id = float(self.Machine_combo_3.currentText())
+            time = float(self.Time_3.toPlainText())
+            step_name = self.Process_name_3.toPlainText()
+            fabrication_step = fabrication_step + 1
+            insert_fabrication_step(step_id,product_id,machine_id,fabrication_step,time,step_name)                
+        
         insert_product(product_id,price,price_brut,name)
 
 if __name__ == "__main__":
